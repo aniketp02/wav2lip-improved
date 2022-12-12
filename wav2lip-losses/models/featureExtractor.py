c30129b766f9584547bbe878a642aeff895deaa4
aniketp02 <pokleaniket92@gmail.com>
2022-12-12 05:04:29 +0000

    Perceptual loss added

---
+++ wav2lip-losses/models/featureExtractor.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torchvision.models import vgg19
+
+class FeatureExtractor(nn.Module):
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+        vgg19_model = vgg19(pretrained=True)
+        self.vgg19_54 = nn.Sequential(*list(vgg19_model.features.children())[:35])
+
+    def forward(self, img):
+        return self.vgg19_54(img)
